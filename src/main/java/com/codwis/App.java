dcb0ed77691cc06f36accc2a3f87d84138172b74
superman <975975629@qq.com>
2018-02-07 08:15:25 +0800

    1

---
+++ src/main/java/com/codwis/App.java
@@ -0,0 +1,14 @@
+package com.codwis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("nihao");
+        System.out.println( "Hello World!" );
+    }
+}
